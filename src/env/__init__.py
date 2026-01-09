8c66f8b0976f823a0e6c3065b3c2c7a32b83a984
Yi Z <yizhao0313@gmail.com>
2026-01-09 15:54:06 +0000

    added inhand rotation env

--- src/env/__init__.py
+++ src/env/__init__.py
@@ -8,11 +8,11 @@ import gymnasium as gym
 
 
 gym.register(
-    id="Template-D2h-v0",
+    id="AnyGrasp-v0",
     entry_point="isaaclab.envs:ManagerBasedRLEnv",
     disable_env_checker=True,
     kwargs={
-        "env_cfg_entry_point": f"{__name__}.tasks.test.env_cfg:D2hEnvCfg",
+        "env_cfg_entry_point": f"{__name__}.tasks.anygrasp.allegro_env_cfg:AllegroCubeEnvCfg",
         # "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
         "rl_games_cfg_entry_point": "/home/yizhao/yi/D2H/src/config/rl_games_ppo_cfg.yaml",
     },
