136fcb1241e8edf072bc7b860ca95d9b496bd0fe
Yi Z <yizhao0313@gmail.com>
2026-07-30 12:26:16 +0100

    slightly reduce the object's size to avoid penstration

--- src/tasks/unscrew/env_cfg.py
+++ src/tasks/unscrew/env_cfg.py
@@ -57,7 +57,8 @@ class SceneCfg(InteractiveSceneCfg):
         prim_path="{ENV_REGEX_NS}/Object",
         spawn=sim_utils.UsdFileCfg(
             usd_path=f"{UWLAB_CLOUD_ASSETS_DIR}/Props/FurnitureBench/SquareLeg/square_leg.usd",
-            scale=(ASSET_SCALE*0.9, ASSET_SCALE*0.9, ASSET_SCALE*0.9),
+            scale=(ASSET_SCALE*0.98, ASSET_SCALE*0.98, ASSET_SCALE*0.98),
+            # scale=(ASSET_SCALE, ASSET_SCALE, ASSET_SCALE),
             rigid_props=sim_utils.RigidBodyPropertiesCfg(
                 solver_position_iteration_count=16,
                 solver_velocity_iteration_count=2,
